3d10330ec256c5c613c6d26b1f311fa755499d6a
ahmeddrabeii <ahma456ad@gmail.com>
2026-09-05 07:52:00 +0300

    Questions02

--- Assignments-OOP/Program.cs
+++ Assignments-OOP/Program.cs
@@ -7,10 +7,21 @@
             #region Part 01 — Theoretical Questions
             #region Questions01
             //a:Abstraction is the process of hiding unnecessary implementation details and showing only the essential features of an object.
-            
+
             //b:Abstraction is one of the four pillars of OOP because it reduces complexity by hiding implementation details and exposing only the essential functionality. This makes programs easier to understand, use, and maintain.
+            #endregion
+
+            #region Questions02
+            //a:
+            //An abstract class is a base class that cannot be instantiated directly and can contain both abstract methods and implemented methods.
+            //An interface defines a contract that a class must follow.A class can implement multiple interfaces, while it can inherit from only one abstract class.
+
+            //b:I would choose an interface when I want to define a common contract or behavior that can be implemented by different and unrelated classes. Interfaces are also useful when a class needs to implement multiple behaviors.
+
+            //c:No, a class cannot inherit from multiple abstract classes because C# supports single class inheritance.  Yes, a class can implement multiple interfaces.
+
             #endregion
             #endregion
         }
-    }
+}
 }
