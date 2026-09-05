98b06c75045b1328cbe80a90ffed39de99f66113
ahmeddrabeii <ahma456ad@gmail.com>
2026-09-05 06:45:42 +0300

    Question 1

--- Assignments-OOP/Program.cs
+++ Assignments-OOP/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 — Theoretical Questions
+            #region Question 1
+            //a:
+            //Method Overloading: It occurs when multiple methods have the same name but different parameters in the same class.
+            //Method Overriding: It occurs when a derived class provides a new implementation for a method inherited from the base class.
+            //b:
+            //Static Binding: The method call is resolved at compile time. It is commonly associated with method overloading.
+            //Dynamic Binding: The method call is resolved at runtime.It is commonly associated with method overriding and virtual methods.
+
+
+            #endregion
+            #endregion
         }
-    }
+}
 }
