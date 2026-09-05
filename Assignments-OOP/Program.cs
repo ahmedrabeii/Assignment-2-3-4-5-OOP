f259e28a0fd3571019f15e5b53c39c6a12cd9ebf
ahmeddrabeii <ahma456ad@gmail.com>
2026-09-05 07:48:59 +0300

    Questions01

--- Assignments-OOP/Program.cs
+++ Assignments-OOP/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 — Theoretical Questions
+            #region Questions01
+            //a:Abstraction is the process of hiding unnecessary implementation details and showing only the essential features of an object.
+            
+            //b:Abstraction is one of the four pillars of OOP because it reduces complexity by hiding implementation details and exposing only the essential functionality. This makes programs easier to understand, use, and maintain.
+            #endregion
+            #endregion
         }
     }
 }
