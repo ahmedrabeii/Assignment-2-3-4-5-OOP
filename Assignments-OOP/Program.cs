bc068a64c8fa62dd22ad060dff95add399d843c9
ahmeddrabeii <ahma456ad@gmail.com>
2026-09-05 08:20:43 +0300

    Questions 01

--- Assignments-OOP/Program.cs
+++ Assignments-OOP/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 — Theoretical Questions
+            #region Questions 01 Object Copying
+            //a:When one object variable is assigned to another, both variables refer to the same object in memory.
+
+            //b:No. Assigning one object variable to another does not create a new object. It copies the reference, so both variables refer to the same object.
+
+            //c:
+            //Copying a reference means that two variables refer to the same object.
+            //Copying an object means creating a separate object with copied data, so the two objects are independent.
+
+            #endregion
+            #endregion
         }
     }
 }
